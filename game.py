3decf4b6a6b0e4380502f1ad339c7ec3a904e71b
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-05 19:30:23 -0400

    Simplify valid options

--- game.py
+++ game.py
@@ -14,22 +14,25 @@ print("USER CHOICE:",user_choice)
 
 # VALIDATE INPUTS
 
+options = ["rock", "paper", "scissors"]
 
 
 
-if user_choice not in["rock", "paper", "scissors"]:
+if user_choice not in options:
     print("INVALID SELECTION. PLEASE TRY IT AGAIN")
     exit()
 
 
 # GENERATE COMPUTER SELECTION
 
-computer_choice = random.choice(["rock", "Paper", "scissors"])
+computer_choice = random.choice(options)
 
 print("---------")
 print("GENERATING...")
 print("COMPUTER CHOICE:",computer_choice)
 
 # DETERMINE THE WINNER
+
+
 
 # DISPLAY FINAL OUTPUTS/OUTCOMES
