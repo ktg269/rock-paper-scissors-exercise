ed1eea5144dce0afd39f43dabf3545b448ab2144
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-05 20:22:03 -0400

    A working version copy

--- game.py
+++ game.py
@@ -2,34 +2,30 @@
 
 import random
 
-
 print("Rock, Paper, Scissors, Shoot!") # this is also a comment
 
 # CAPTURE INPUTS
 
-
 user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
-print("---------")
-print("USER CHOICE:",user_choice)
+
+print("--------------")
+print("USER CHOICE:", user_choice)
 
 # VALIDATE INPUTS
 
 options = ["rock", "paper", "scissors"]
 
-
-
 if user_choice not in options:
-    print("INVALID SELECTION. PLEASE TRY IT AGAIN")
+    print("INVALID SELECTION, PLEASE TRY AGAIN...")
     exit()
-
 
 # GENERATE COMPUTER SELECTION
 
 computer_choice = random.choice(options)
 
-print("---------")
+print("--------------")
 print("GENERATING...")
-print("COMPUTER CHOICE:",computer_choice)
+print("COMPUTER CHOICE:", computer_choice)
 
 # DETERMINE THE WINNER
 #
@@ -38,21 +34,35 @@ print("COMPUTER CHOICE:",computer_choice)
 # scissors beats paper
 # same selections is a tie
 #
+# first attribute represents the user, second represents the computer
+winners = {
+    "rock":{
+        "rock": None,
+        "paper": "paper",
+        "scissors": "rock",
+    },
+    "paper":{
+        "rock": "paper",
+        "paper": None,
+        "scissors": "scissors",
+    },
+    "scissors":{
+        "rock": "rock",
+        "paper": "scissors",
+        "scissors": None,
+    },
+}
 
-if user_choice == computer_choice:
+winning_choice = winners[user_choice][computer_choice]
+
+# DISPLAY FINAL OUTPUTS / OUTCOMES
+
+if winning_choice:
+    if winning_choice == user_choice:
+        print("YOU WON")
+    elif winning_choice == computer_choice:
+        print("YOU LOST")
+else:
     print("TIE")
-elif user_choice =="rock" and computer_choice == "paper":
-    print("WINNER IS PAPER")
-    print("COMPUTER WINS")
-elif user_choice =="rock" and computer_choice == "scissors":
-    print("ROCK")
-elif user_choice =="paper" and computer_choice == "rock":
-    print("PAPER")
-elif user_choice =="paper" and computer_choice == "scissors":
-    print("SCISSORS")
-elif user_choice =="scissors" and computer_choice == "rock":
-    print("ROCK")
-elif user_choice =="scissors" and computer_choice == "paper":
-    print("SCISSORS")
-# DISPLAY FINAL OUTPUTS/OUTCOMES
 
+print("Thanks for playing. Please play again!")
