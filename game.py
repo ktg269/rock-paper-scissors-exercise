b37010bf0dbfaca3cde8a7605823196834e3ee4c
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-05 20:58:38 -0400

    Implement example test (not working)

--- game.py
+++ game.py
@@ -2,67 +2,95 @@
 
 import random
 
-print("Rock, Paper, Scissors, Shoot!") # this is also a comment
+def my_message():
+    return "HELLO"
 
-# CAPTURE INPUTS
+def determine_winner(u, c):
+    winners = {
+        "rock":{
+            "rock": None,
+            "paper": "paper",
+            "scissors": "rock",
+        },
+        "paper":{
+            "rock": "paper",
+            "paper": None,
+            "scissors": "scissors",
+        },
+        "scissors":{
+            "rock": "rock",
+            "paper": "scissors",
+            "scissors": None,
+        },
+    }
 
-user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
+        winning_choice = winners[u][c]
+        return winning_choice #"rock"
 
-print("--------------")
-print("USER CHOICE:", user_choice)
+# only if this script is executed from the command-line
+if __name__ == "__main__":
 
-# VALIDATE INPUTS
+    print("Rock, Paper, Scissors, Shoot!") # this is also a comment
 
-options = ["rock", "paper", "scissors"]
+    # CAPTURE INPUTS
 
-if user_choice not in options:
-    print("INVALID SELECTION, PLEASE TRY AGAIN...")
-    exit()
+    user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
 
-# GENERATE COMPUTER SELECTION
+    print("--------------")
+    print("USER CHOICE:", user_choice)
 
-computer_choice = random.choice(options)
+    # VALIDATE INPUTS
 
-print("--------------")
-print("GENERATING...")
-print("COMPUTER CHOICE:", computer_choice)
+    options = ["rock", "paper", "scissors"]
 
-# DETERMINE THE WINNER
-#
-# rock beats scissors
-# paper beats rock
-# scissors beats paper
-# same selections is a tie
-#
-# first attribute represents the user, second represents the computer
-winners = {
-    "rock":{
-        "rock": None,
-        "paper": "paper",
-        "scissors": "rock",
-    },
-    "paper":{
-        "rock": "paper",
-        "paper": None,
-        "scissors": "scissors",
-    },
-    "scissors":{
-        "rock": "rock",
-        "paper": "scissors",
-        "scissors": None,
-    },
-}
+    if user_choice not in options:
+        print("INVALID SELECTION, PLEASE TRY AGAIN...")
+        exit()
 
-winning_choice = winners[user_choice][computer_choice]
+    # GENERATE COMPUTER SELECTION
 
-# DISPLAY FINAL OUTPUTS / OUTCOMES
+    computer_choice = random.choice(options)
 
-if winning_choice:
-    if winning_choice == user_choice:
-        print("YOU WON")
-    elif winning_choice == computer_choice:
-        print("YOU LOST")
-else:
-    print("TIE")
+    print("--------------")
+    print("GENERATING...")
+    print("COMPUTER CHOICE:", computer_choice)
 
-print("Thanks for playing. Please play again!")
+    # DETERMINE THE WINNER
+    #
+    # rock beats scissors
+    # paper beats rock
+    # scissors beats paper
+    # same selections is a tie
+    #
+    # first attribute represents the user, second represents the computer
+    winners = {
+        "rock":{
+            "rock": None,
+            "paper": "paper",
+            "scissors": "rock",
+        },
+        "paper":{
+            "rock": "paper",
+            "paper": None,
+            "scissors": "scissors",
+        },
+        "scissors":{
+            "rock": "rock",
+            "paper": "scissors",
+            "scissors": None,
+        },
+    }
+
+    winning_choice = winners[user_choice][computer_choice]
+
+    # DISPLAY FINAL OUTPUTS / OUTCOMES
+
+    if winning_choice:
+        if winning_choice == user_choice:
+            print("YOU WON")
+        elif winning_choice == computer_choice:
+            print("YOU LOST")
+    else:
+        print("TIE")
+
+    print("Thanks for playing. Please play again!")
